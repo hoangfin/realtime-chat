7f94894e2e9bd6ee8dce4056f5cc6df00ac18808
hoangfin <hoang.tran.fin@gmail.com>
2025-01-08 00:52:34 +0200

    Exported firebase auth, db and storage

--- firebase.ts
+++ firebase.ts
@@ -1,4 +1,7 @@
 import { FirebaseOptions, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = {
 	apiKey: import.meta.env.VITE_API_KEY,
@@ -10,3 +13,7 @@ const firebaseConfig: FirebaseOptions = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const db = getFirestore();
+export const storage = getStorage();
