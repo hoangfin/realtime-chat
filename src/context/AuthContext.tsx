6e63b409ab96ff3a5747720312934587b1435999
hoangfin <hoang.tran.fin@gmail.com>
2025-01-10 01:30:27 +0200

    Revising User type returned from register|login

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,22 +1,61 @@
 import { createContext, PropsWithChildren, useContext, useState } from "react";
-import { UserInfo } from "firebase/auth";
+import { auth } from "@src/lib/firebase";
+import {
+	createUserWithEmailAndPassword,
+	User,
+	signInWithEmailAndPassword,
+	signOut
+} from "firebase/auth";
 
 type AuthContextProps = {
-	user: UserInfo | null;
-	login: (user: UserInfo) => void;
-	logout: () => void;
+	user: User | null;
+	loading: boolean;
+	register: (email: string, password: string) => Promise<User>;
+	login: (email: string, password: string) => Promise<User>;
+	logout: () => Promise<void>;
 };
 
 const AuthContext = createContext<AuthContextProps | undefined>(undefined);
 
 export function AuthProvider({ children }: PropsWithChildren) {
-	const [user, setUser] = useState<UserInfo | null>(null);
+	const [user, setUser] = useState<User | null>(null);
+	const [loading, setLoading] = useState<boolean>(false);
 
-	const login = (user: UserInfo) => setUser(user);
-	const logout = () => setUser(null);
+	const register = async (email: string, password: string) => {
+		setLoading(true);
+		try {
+			const { user } = await createUserWithEmailAndPassword(auth, email, password);
+			setUser(user);
+			return user;
+		} catch (error) {
+			throw error;
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const login = async (email: string, password: string) => {
+		setLoading(true);
+
+		try {
+			const { user } = await signInWithEmailAndPassword(auth, email, password);
+			setUser(user);
+			return user;
+		} catch (error) {
+			throw error;
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	const logout = async () => {
+		setLoading(true);
+		await signOut(auth);
+		setLoading(false);
+	};
 
 	return (
-		<AuthContext.Provider value={{ user, login, logout }}>
+		<AuthContext.Provider value={{ user, loading, register, login, logout }}>
 			{children}
 		</AuthContext.Provider>
 	);
@@ -24,5 +63,10 @@ export function AuthProvider({ children }: PropsWithChildren) {
 
 export function useAuth(): AuthContextProps | undefined {
 	const context = useContext(AuthContext);
+
+	if (!context) {
+		throw new Error("Failed to create context for useAuth hook");
+	}
+
 	return context;
 }
