26363096ef961b01b156370495f2415c2a3da634
hoangfin <hoang.tran.fin@gmail.com>
2025-01-13 01:28:41 +0200

    Registration worked

--- src/components/pages/AuthPage/RegisterForm.tsx
+++ src/components/pages/AuthPage/RegisterForm.tsx
@@ -1,18 +1,21 @@
 import { LoadingButton } from "@mui/lab";
 import { Stack, StackProps, TextField, Typography } from "@mui/material";
+import { useAuth } from "@src/stores/AuthStore";
 import { FormEvent } from "react";
 
 export function RegisterForm(props: StackProps): JSX.Element {
+	const register = useAuth((state) => state.register);
+	const isRegistering = useAuth((state) => state.isRegistering);
 
-	const handleRegister = (event: FormEvent<HTMLFormElement>) => {
+	const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		const formData = new FormData(event.currentTarget);
 		const { username, email, password } = Object.fromEntries(formData);
 
 		try {
-
+			await register(username as string, email as string, password as string);
 		} catch (error) {
-
+			console.log(error);
 		}
 	};
 
@@ -24,7 +27,7 @@ export function RegisterForm(props: StackProps): JSX.Element {
 				<TextField type="text" placeholder="Username" name="username" />
 				<TextField type="email" placeholder="Email" name="email" required />
 				<TextField type="password" placeholder="Password" name="password" required />
-				<LoadingButton type="submit" variant="contained">
+				<LoadingButton type="submit" loading={isRegistering} variant="contained">
 					Sign Up
 				</LoadingButton>
 			</Stack>
