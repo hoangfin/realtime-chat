e2db210f7058366e3d3670750b3e0112ed38c6a9
hoangfin <hoang.tran.fin@gmail.com>
2025-01-31 02:46:05 +0200

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,28 @@ import { SxProps } from "@mui/material";
 import { RegisterPage } from "@src/components/pages";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
-import { Route, Routes } from "react-router";
+import { Link, Route, Routes } from "react-router";
 import { LoginPage } from "./components/pages/LoginPage";
 import { auth, getUser } from "./services/firebase";
-import { useAuth } from "./stores/AuthStore";
+import { useAuth } from "./stores/authStore";
 
-const containerSx: SxProps = {
-	width: "80vw",
-	minHeight: "90vh",
-	margin: "5vh auto 5vh auto",
-	padding: 0,
-	display: "flex",
-	backgroundColor: "rgba(17, 25, 40, 0.75)",
-	backdropFilter: "blur(19px) saturate(180%)",
-	border: "1px solid rgba(255, 255, 255, 0.125)",
-	borderRadius: "12px"
-};
+// const containerSx: SxProps = {
+// 	width: "80vw",
+// 	minHeight: "90vh",
+// 	margin: "5vh auto 5vh auto",
+// 	padding: 0,
+// 	display: "flex",
+// 	backgroundColor: "rgba(17, 25, 40, 0.75)",
+// 	backdropFilter: "blur(19px) saturate(180%)",
+// 	border: "1px solid rgba(255, 255, 255, 0.125)",
+// 	borderRadius: "12px"
+// };
 
 useAuth.getInitialState().isLoggingIn = true;
 
 export default function App() {
 	const isLoggingIn = useAuth((state) => state.isLoggingIn);
-	const setIsLoggingIn = useAuth((state) => state.setIsLoggingIn);
+	const setLoggingIn = useAuth((state) => state.setLoggingIn);
 	const setUser = useAuth((state) => state.setUser);
 
 	console.log(`isLoggingIn = ${isLoggingIn}`);
@@ -39,13 +39,13 @@ export default function App() {
 				}
 			}
 			console.log("setIsLoggingIn()");
-			setIsLoggingIn(false);
+			setLoggingIn(false);
 		});
 
 		return () => {
 			unsubscribe();
 		};
-	}, [onAuthStateChanged, getUser, setUser, setIsLoggingIn]);
+	}, [onAuthStateChanged, getUser, setUser, setLoggingIn]);
 
 	if (isLoggingIn) {
 		return <h1>...Loading</h1>;
@@ -53,7 +53,7 @@ export default function App() {
 
 	return (
 		<Routes>
-			<Route path="/" element={<div>Home</div>} />
+			<Route path="/" element={<div><Link to="/login">login</Link></div>} />
 			<Route path="/login" element={<LoginPage />} />
 			<Route path="/register" element={<RegisterPage />} />
 			<Route path="*" />
