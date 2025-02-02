9da01cf6ddb2d3ff8c0ad14caae024f9686b5fbe
hoangfin <hoang.tran.fin@gmail.com>
2025-02-02 19:47:45 +0200

    Used createBrowserRouter() for data route

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,27 @@
-import { SxProps } from "@mui/material";
-import { RegisterPage } from "@src/components/pages";
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { Link, Route, Routes } from "react-router";
-import { LoginPage } from "./components/pages/LoginPage";
-import { auth, getUser } from "./services/firebase";
-import { useAuth } from "./stores/authStore";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider
+} from "react-router-dom";
 
-// const containerSx: SxProps = {
-// 	width: "80vw",
-// 	minHeight: "90vh",
-// 	margin: "5vh auto 5vh auto",
-// 	padding: 0,
-// 	display: "flex",
-// 	backgroundColor: "rgba(17, 25, 40, 0.75)",
-// 	backdropFilter: "blur(19px) saturate(180%)",
-// 	border: "1px solid rgba(255, 255, 255, 0.125)",
-// 	borderRadius: "12px"
-// };
+import { HomePage, LoginPage, RegisterPage } from "@src/components/pages";
+import { authLoader } from "@src/loaders";
 
-useAuth.getInitialState().isLoggingIn = true;
-
-export default function App() {
-	const isLoggingIn = useAuth((state) => state.isLoggingIn);
-	const setLoggingIn = useAuth((state) => state.setLoggingIn);
-	const setUser = useAuth((state) => state.setUser);
-
-	console.log(`isLoggingIn = ${isLoggingIn}`);
-
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
-			if (authUser) {
-				try {
-					const user = await getUser(authUser.uid);
-					setUser(user);
-				} catch (error) {
-
-				}
-			}
-			console.log("setIsLoggingIn()");
-			setLoggingIn(false);
-		});
-
-		return () => {
-			unsubscribe();
-		};
-	}, [onAuthStateChanged, getUser, setUser, setLoggingIn]);
-
-	if (isLoggingIn) {
-		return <h1>...Loading</h1>;
-	}
-
-	return (
-		<Routes>
-			<Route path="/" element={<div><Link to="/login">login</Link></div>} />
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route loader={authLoader}>
+			<Route path="/" element={<HomePage />} />
 			<Route path="/login" element={<LoginPage />} />
 			<Route path="/register" element={<RegisterPage />} />
 			<Route path="*" />
-		</Routes>
+		</Route>
+	),
+	{ basename: "/" }
+);
+
+export default function App() {
+	return (
+		<RouterProvider router={router} />
 	);
 };
