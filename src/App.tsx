aa6c559bfc71d13a430b9181b0bbb70e3d44f3db
hoangfin <hoang.tran.fin@gmail.com>
2025-02-02 19:57:36 +0200

    Simplified with RouterProvider and imported router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,5 @@
-import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider
-} from "react-router-dom";
-
-import { HomePage, LoginPage, RegisterPage } from "@src/components/pages";
-import { authLoader } from "@src/loaders";
-
-const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route loader={authLoader}>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/login" element={<LoginPage />} />
-			<Route path="/register" element={<RegisterPage />} />
-			<Route path="*" />
-		</Route>
-	),
-	{ basename: "/" }
-);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 export default function App() {
 	return (
