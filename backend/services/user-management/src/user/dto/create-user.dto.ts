ea8a64dbebb186b63596bb19dc3f1f4cbc669248
hoangfin <hoang.tran.fin@gmail.com>
2025-04-13 16:12:51 +0300

    Added validation to all properties

--- backend/services/user-management/src/user/dto/create-user.dto.ts
+++ backend/services/user-management/src/user/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsEmail, IsOptional, IsString, IsUrl, Length, Matches, MinLength } from "class-validator";
+
+export class CreateUserDto {
+	@IsEmail()
+	email!: string;
+
+	@Length(3, 50)
+	@Matches(/^[a-zA-Z0-9_]+$/, {
+		message: "Username can only contain letters, numbers and underscores"
+	})
+	username!: string;
+
+	@IsOptional()
+	@Length(1, 100)
+	name?: string;
+
+	@MinLength(8)
+	password!: string;
+
+	@IsOptional()
+	@IsUrl()
+	avatarUrl?: string;
+}
