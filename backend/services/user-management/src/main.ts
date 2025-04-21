c454a5306f7322bcbbb974ccb04c75482872ddbf
hoangfin <hoang.tran.fin@gmail.com>
2025-04-22 01:57:48 +0300

    Added Swagger

--- backend/services/user-management/src/main.ts
+++ backend/services/user-management/src/main.ts
@@ -1,10 +1,22 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(new ValidationPipe());
+
+	const config = new DocumentBuilder()
+        .setTitle("Users API")
+        .setDescription("API documentation for users")
+        .setVersion("1.0")
+        .addTag("Users")
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("users/documentation", app, document);
+
 	await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
